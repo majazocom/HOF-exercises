c7a2585c6815ade2c7e4f3365dc2f73cf41617f5
Maja Gedda <maja.gedda@zocom.se>
2020-10-26 14:35:30 +0100

    hof

---
+++ script.js
@@ -0,0 +1,63 @@
+users = [
+    {
+      name: 'Zelda',
+      age: 25
+    },
+    {
+      name: 'Peach',
+      age: 32
+    },
+    {
+      name: 'Mew',
+      age: 21
+    },
+    {
+      name: 'Samus',
+      age: 30
+    },
+    {
+      name: 'Roy',
+      age: 16
+    },
+    {
+      name: 'Link',
+      age: 36
+    },
+    {
+      name: 'Captain Falcon',
+      age: 42
+    },
+    {
+      name: 'Ness',
+      age: 12
+    }
+  ];
+
+
+//Del 1 - Bonus = MVG på hela programmet
+//Vilket spel?
+
+//Del 2
+//Mappa ut alla objekten som är över eller lika med 20
+
+//Del 3
+//Mappa ut alla objektens NAMN som är över eller lika med 20
+
+//Del 4
+//Skapa en ny lista med bara namnen och en lista med bara åldrarna
+
+//Del 5
+//Hitta tre karaktärer till från spelet och lägg in dem i listan med
+
+//Del 6
+//Sortera dina karaktärer i namnordning, a - z
+
+//Del 7
+//Sortera dina karaktärer i åldersordning, 1 - 10000år
+
+//Del 8
+//Kolla om en karaktär med namnet 'Mew' finns med i listan.
+
+
+
+  
